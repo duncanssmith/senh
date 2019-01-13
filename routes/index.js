8f70edd934efa5db3573d053ddb8fd0873011964
Duncan Smith <duncanssmith@gmail.com>
2019-01-13 10:57:45 +0000

    Displaying an image

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', info: 'Some string', testString1: 'More stuff here' });
+  res.render('index', { title: 'PalmC', youAreUsingJade: true, imagePath: '/images/esperanza_spalding.jpg' });
 });
 
 module.exports = router;
