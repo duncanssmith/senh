bb4968b832f589735507d1d5fb19c6067e80a642
Duncan Smith <duncanssmith@gmail.com>
2019-01-17 17:34:03 +0000

    Start creating pages 1 hour

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PalmC', youAreUsingJade: true, imagePath: '/images/esperanza_spalding.jpg' });
+  res.render('index', { title: 'PalmC', youAreUsingJade: true, imagePath: '/images/esperanza_spalding.jpg', image: 'esperanza_spalding.jpg' });
+});
+
+/* GET overview page. */
+router.get('/p1_overview', function(req, res, next) {
+  res.render('p1_overview', { title: 'PalmC Overview', youAreUsingJade: true, imagePath: '/images/esperanza_spalding.jpg' });
+});
+
+/* GET consultation page. */
+router.get('/p2_consultation', function(req, res, next) {
+  res.render('p2_consultation', { title: 'PalmC Consultation', youAreUsingJade: true, imagePath: '/images/esperanza_spalding.jpg' });
+});
+
+/* GET consultation page. */
+router.get('/p3_otherServices', function(req, res, next) {
+  res.render('p3_otherServices', { title: 'PalmC Other services' });
+});
+
+/* GET consultation page. */
+router.get('/p4_packages', function(req, res, next) {
+  res.render('p4_packages', { title: 'PalmC Packages' });
 });
 
 module.exports = router;
