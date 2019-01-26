fe7e8de6f3218cf1fac3935eb3bd46691bbdb46c
Duncan Smith <duncanssmith@gmail.com>
2019-01-26 17:38:21 +0000

    Change PalmC to Senh

--- routes/index.js
+++ routes/index.js
@@ -3,117 +3,117 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('p0_ourService', { title: 'PalmC Our service' });
+  res.render('p0_ourService', { title: 'Senh: Our service' });
 });
 
 /* GET overview page. */
 router.get('/p00_overview', function(req, res, next) {
-  res.render('p00_overview', { title: 'PalmC Overview of dietetics' });
+  res.render('p00_overview', { title: 'Senh: Overview of dietetics' });
 });
 
 /* GET overview page. */
 router.get('/p01_consultation', function(req, res, next) {
-  res.render('p01_consultation', { title: 'PalmC Consultation' });
+  res.render('p01_consultation', { title: 'Senh: Consultation' });
 });
 
 /* GET overview page. */
 router.get('/p02_otherServices', function(req, res, next) {
-  res.render('p02_otherServices', { title: 'PalmC Other services' });
+  res.render('p02_otherServices', { title: 'Senh: Other services' });
 });
 
 /* GET overview page. */
 router.get('/p03_packages', function(req, res, next) {
-  res.render('p03_packages', { title: 'PalmC Packages' });
+  res.render('p03_packages', { title: 'Senh: Packages' });
 });
 
 /* GET overview page. */
 router.get('/p1_yourHealth', function(req, res, next) {
-  res.render('p1_yourHealth', { title: 'PalmC Your health' });
+  res.render('p1_yourHealth', { title: 'Senh: Your health' });
 });
 
 /* GET overview page. */
 router.get('/p10_diabetes', function(req, res, next) {
-  res.render('p10_diabetes', { title: 'PalmC Diabetes' });
+  res.render('p10_diabetes', { title: 'Senh: Diabetes' });
 });
 
 /* GET overview page. */
 router.get('/p11_heartHealth', function(req, res, next) {
-  res.render('p11_heartHealth', { title: 'PalmC Heart health' });
+  res.render('p11_heartHealth', { title: 'Senh: Heart health' });
 });
 
 /* GET overview page. */
 router.get('/p12_IBS', function(req, res, next) {
-  res.render('p12_IBS', { title: 'PalmC Irritable bowel syndrome - IBS' });
+  res.render('p12_IBS', { title: 'Senh: Irritable bowel syndrome - IBS' });
 });
 
 /* GET overview page. */
 router.get('/p13_mineralVitamins', function(req, res, next) {
-  res.render('p13_mineralVitamins', { title: 'PalmC Minerals and vitamins ' });
+  res.render('p13_mineralVitamins', { title: 'Senh: Minerals and vitamins ' });
 });
 
 /* GET overview page. */
 router.get('/p14_allergy', function(req, res, next) {
-  res.render('p14_allergy', { title: 'PalmC Food intolerance and allergies' });
+  res.render('p14_allergy', { title: 'Senh: Food intolerance and allergies' });
 });
 
 /* GET overview page. */
 router.get('/p15_weightLoss', function(req, res, next) {
-  res.render('p15_weightLoss', { title: 'PalmC Weight Loss' });
+  res.render('p15_weightLoss', { title: 'Senh: Weight Loss' });
 });
 
 /* GET overview page. */
 router.get('/p16_families', function(req, res, next) {
-  res.render('p16_families', { title: 'PalmC Families' });
+  res.render('p16_families', { title: 'Senh: Families' });
 });
 
 /* GET overview page. */
 router.get('/p17_veg', function(req, res, next) {
-  res.render('p17_veg', { title: 'PalmC Vegetetarian and Vegan' });
+  res.render('p17_veg', { title: 'Senh: Vegetetarian and Vegan' });
 });
 
 /* GET overview page. */
 router.get('/p18_cancer', function(req, res, next) {
-  res.render('p18_cancer', { title: 'PalmC Cancer' });
+  res.render('p18_cancer', { title: 'Senh: Cancer' });
 });
 
 /* GET overview page. */
 router.get('/p19_coeliac', function(req, res, next) {
-  res.render('p19_coeliac', { title: 'PalmC Coeliac' });
+  res.render('p19_coeliac', { title: 'Senh: Coeliac' });
 });
 
 /* GET overview page. */
 router.get('/p1A_PCOS', function(req, res, next) {
-  res.render('p1A_PCOS', { title: 'PalmC PCOS' });
+  res.render('p1A_PCOS', { title: 'Senh: PCOS' });
 });
 
 /* GET overview page. */
 router.get('/p1B_elderly', function(req, res, next) {
-  res.render('p1B_elderly', { title: 'PalmC Elderly dietary needs' });
+  res.render('p1B_elderly', { title: 'Senh: Elderly dietary needs' });
 });
 
 /* GET overview page. */
 router.get('/p1C_vitality', function(req, res, next) {
-  res.render('p1C_vitality', { title: 'PalmC Vitality' });
+  res.render('p1C_vitality', { title: 'Senh: Vitality' });
 });
 
 /* GET consultation page. */
 router.get('/p2_yourOrganisation', function(req, res, next) {
-  res.render('p2_yourOrganisation', { title: 'PalmC Your organisation' });
+  res.render('p2_yourOrganisation', { title: 'Senh: Your organisation' });
 });
 
 /* GET consultation page. */
 router.get('/p3_about', function(req, res, next) {
-  res.render('p3_about', { title: 'PalmC About' });
+  res.render('p3_about', { title: 'Senh: About' });
 });
 
 /* GET consultation page. */
 router.get('/p4_contact', function(req, res, next) {
-  res.render('p4_contact', { title: 'PalmC Contact' });
+  res.render('p4_contact', { title: 'Senh: Contact' });
 });
 
 /* GET consultation page. */
 router.get('/p5_enquiry', function(req, res, next) {
-  res.render('p5_enquiry', { title: 'PalmC Enquiry' });
+  res.render('p5_enquiry', { title: 'Senh: Enquiry' });
 });
 
 module.exports = router;
