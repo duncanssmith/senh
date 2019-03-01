114eaea17c85cb806d9e9dbaf5db020c4857842b
Duncan Smith <duncanssmith@gmail.com>
2019-03-01 09:51:05 +0000

    Rename and use structure template

--- routes/index.js
+++ routes/index.js
@@ -3,117 +3,112 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('p0_ourService', { title: 'Senh: Our service' });
+  res.render('000_service', { title: 'Senh: Our service' });
 });
 
 /* GET overview page. */
 router.get('/Overview', function(req, res, next) {
-  res.render('p00_overview', { title: 'Senh: Overview of dietetics' });
+  res.render('010_overview', { title: 'Senh: Overview of dietetics' });
 });
 
 /* GET overview page. */
 router.get('/Consultation', function(req, res, next) {
-  res.render('p01_consultation', { title: 'Senh: Consultation' });
+  res.render('011_consultation', { title: 'Senh: Consultation' });
 });
 
 /* GET overview page. */
 router.get('/OtherServices', function(req, res, next) {
-  res.render('p02_otherServices', { title: 'Senh: Other services' });
+  res.render('012_services', { title: 'Senh: Other services' });
 });
 
 /* GET overview page. */
 router.get('/Packages', function(req, res, next) {
-  res.render('p03_packages', { title: 'Senh: Packages' });
+  res.render('013_packages', { title: 'Senh: Packages' });
 });
 
 /* GET overview page. */
 router.get('/YourHealth', function(req, res, next) {
-  res.render('p1_yourHealth', { title: 'Senh: Your health' });
+  res.render('001_health', { title: 'Senh: Your health' });
 });
 
 /* GET overview page. */
 router.get('/Diabetes', function(req, res, next) {
-  res.render('p10_diabetes', { title: 'Senh: Diabetes' });
+  res.render('100_diabetes', { title: 'Senh: Diabetes' });
 });
 
 /* GET overview page. */
 router.get('/HeartHealth', function(req, res, next) {
-  res.render('p11_heartHealth', { title: 'Senh: Heart health' });
+  res.render('101_heart', { title: 'Senh: Heart health' });
 });
 
 /* GET overview page. */
 router.get('/IBS', function(req, res, next) {
-  res.render('p12_IBS', { title: 'Senh: Irritable bowel syndrome - IBS' });
+  res.render('102_ibs', { title: 'Senh: Irritable bowel syndrome - IBS' });
 });
 
 /* GET overview page. */
 router.get('/MineralsVitamins', function(req, res, next) {
-  res.render('p13_mineralVitamins', { title: 'Senh: Minerals and vitamins ' });
+  res.render('103_vitamins', { title: 'Senh: Minerals and vitamins ' });
 });
 
 /* GET overview page. */
 router.get('/Allergies', function(req, res, next) {
-  res.render('p14_allergy', { title: 'Senh: Food intolerance and allergies' });
+  res.render('104_allergies', { title: 'Senh: Food intolerance and allergies' });
 });
 
 /* GET overview page. */
 router.get('/WeightLoss', function(req, res, next) {
-  res.render('p15_weightLoss', { title: 'Senh: Weight Loss' });
+  res.render('105_weight', { title: 'Senh: Weight Loss' });
 });
 
 /* GET overview page. */
 router.get('/Families', function(req, res, next) {
-  res.render('p16_families', { title: 'Senh: Families' });
+  res.render('106_families', { title: 'Senh: Families' });
 });
 
 /* GET overview page. */
 router.get('/VeganVegetarian', function(req, res, next) {
-  res.render('p17_veg', { title: 'Senh: Vegetetarian and Vegan' });
+  res.render('107_vegetarian', { title: 'Senh: Vegetetarian and Vegan' });
 });
 
 /* GET overview page. */
 router.get('/Cancer', function(req, res, next) {
-  res.render('p18_cancer', { title: 'Senh: Cancer' });
+  res.render('108_cancer', { title: 'Senh: Cancer' });
 });
 
 /* GET overview page. */
 router.get('/Coeliac', function(req, res, next) {
-  res.render('p19_coeliac', { title: 'Senh: Coeliac' });
+  res.render('109_coeliac', { title: 'Senh: Coeliac' });
 });
 
 /* GET overview page. */
 router.get('/PCOS', function(req, res, next) {
-  res.render('p1A_PCOS', { title: 'Senh: PCOS' });
+  res.render('110_pcos', { title: 'Senh: PCOS' });
 });
 
 /* GET overview page. */
 router.get('/NutritionForTheElderly', function(req, res, next) {
-  res.render('p1B_elderly', { title: 'Senh: Elderly dietary needs' });
+  res.render('111_elderly', { title: 'Senh: Elderly dietary needs' });
 });
 
 /* GET overview page. */
 router.get('/Vitality', function(req, res, next) {
-  res.render('p1C_vitality', { title: 'Senh: Vitality' });
+  res.render('112_vitality', { title: 'Senh: Vitality' });
 });
 
 /* GET consultation page. */
 router.get('/YourOrganisation', function(req, res, next) {
-  res.render('p2_yourOrganisation', { title: 'Senh: Your organisation' });
+  res.render('002_organisation', { title: 'Senh: Your organisation' });
 });
 
 /* GET consultation page. */
 router.get('/About', function(req, res, next) {
-  res.render('p3_about', { title: 'Senh: About' });
+  res.render('003_about', { title: 'Senh: About' });
 });
 
 /* GET consultation page. */
 router.get('/Contact', function(req, res, next) {
-  res.render('p4_contact', { title: 'Senh: Contact' });
-});
-
-/* GET consultation page. */
-router.get('/Proto', function(req, res, next) {
-  res.render('000_prototype', { title: 'Proto' });
+  res.render('004_contact', { title: 'Senh: Contact' });
 });
 
 module.exports = router;
